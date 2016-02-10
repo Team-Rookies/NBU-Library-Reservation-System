87e2b5b118fb077b2ae50f14c6ec073c760675a5
Bogomil <bogomil.dimitrov@yahoo.com>
2016-02-10 23:58:37 +0200

    Admin panel grid now works with all calendars that are selected from a select field

--- template/js/gridApp.js
+++ template/js/gridApp.js
@@ -1,29 +1,58 @@
 var grid = grid || {};
 
 $(function () {
-    var data;
-
-    $.getJSON('../jsonDB/collectiveEvents.json', function(json) {
-        data = json.result;
-    }).then(function() {
-        grid.data = data;
-        $("#jsGrid").jsGrid({
-            theme: 'classic',
-            height: "auto",
-            width: "100%",
-            sorting: true,
-            paging: true,
-            autoload: true,
-            controller: grid.gridController,
-            fields: [
-                {name: "id", type: "text", title:"Id", width:50, sorting: true},
-                {name: "title", type: "textarea", width: 150, title:'Title', sorting: true},
-                {name: 'department', type: 'textarea', width: 150, title:'Department', sorting: true},
-                {name: 'multimedia', type: "checkbox", title: "Needs multimedia", sorting: true },
-                {name: 'additionalInfo', type: 'textarea', width: 150, title:'Additional Information',sorting: false},
-                {name: 'deleteUrl', type:'repeated', title:'Delete Link Id'},
-                {type:'control'}
-            ]
+    $.getJSON('../jsonDB/calendars.json', function(json) {
+        if(json) {
+            if(!sessionStorage['calendarFile']) {
+                sessionStorage['calendarFile'] = calendars[0].fileName;
+                sessionStorage['calendarName'] = calendars[0].name;
+            }
+            $('#hallName').text(sessionStorage['calendarName']);
+            grid.calendars = json.calendars;
+            grid.calendars.forEach(function(c) {
+                $('<option/>').attr({value: c.fileName}).text(c.name).appendTo('#halls');
+            });
+            eventHandlers();
+        } else {
+            throw new Error('Error loading calendars');
+        }
+    }).complete(function(e) {
+        $('#halls').selectpicker({
+            style:'btn-info'
         });
     });
+
+    $('#halls').on('change', function() {
+        var data;
+        var fileName = '../jsonDB/' + this.value;
+
+        $.getJSON(fileName, function(json) {
+            data = json.result;
+        }).then(function() {
+            grid.data = data;
+            $("#jsGrid").jsGrid({
+                theme: 'classic',
+                height: "auto",
+                width: "100%",
+                sorting: true,
+                paging: true,
+                autoload: true,
+                controller: grid.gridController,
+                fields: [
+                    {name: "id", type: "text", title:"Id", width:50, sorting: true},
+                    {name: "title", type: "textarea", width: 150, title:'Title', sorting: true},
+                    {name: 'department', type: 'textarea', width: 150, title:'Department', sorting: true},
+                    {name: 'multimedia', type: "checkbox", title: "Needs multimedia", sorting: true },
+                    {name: 'additionalInfo', type: 'textarea', width: 150, title:'Additional Information',sorting: false},
+                    {name: 'deleteUrl', type:'repeated', title:'Delete Link Id'},
+                    {type:'control'}
+                ]
+            });
+        });
+    });
+
 });
+
+function eventHandlers() {
+
+}
